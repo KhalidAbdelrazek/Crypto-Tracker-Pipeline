7e94188eff1dab8d996a4118eecf0720d5f2f538
Khalid Abdelrazk <khalidabdelrazk990@gmail.com>
2026-04-19 03:41:39 +0200

    fully initialize workflow with venv and all inside the same folder using the update setup.sh

--- airflow_home/dags/my_first_dag.py
+++ airflow_home/dags/my_first_dag.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from airflow.operators.bash import BashOperator
 
 with DAG(
-    dag_id="welcome_to_airflow",
+    dag_id="my_first_dag",
     start_date= None,
     schedule='0 0 * * *',
     catchup=False
